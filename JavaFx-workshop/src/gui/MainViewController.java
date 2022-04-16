f60b07ba6667ae1577c2e210f0a9f50f77f8a4d4
Cesar Augusto <ces.augusto108@gmail.com>
2022-04-16 20:48:29 -0300

    MainView controller

---
+++ JavaFx-workshop/src/gui/MainViewController.java
@@ -0,0 +1,40 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem salespersonMenuItem;
+
+    @FXML
+    private MenuItem departmentMenuItem;
+
+    @FXML
+    private MenuItem aboutMenuItem;
+
+    @FXML
+    public void onSalespersonMenuItemAction() {
+
+    }
+
+    @FXML
+    public void onDepartmentMenuItemAction() {
+
+    }
+
+    @FXML
+    public void onAboutMenuItemAction() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
