850c8af67a3f7fe16ed3ee22b7e14df222b0e414
Cesar Augusto <ces.augusto108@gmail.com>
2022-04-17 18:42:09 -0300

    Department form dialog design

--- JavaFx-workshop/src/gui/DepartmentListController.java
+++ JavaFx-workshop/src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -40,16 +49,9 @@ public class DepartmentListController implements Initializable {
     }
 
     @FXML
-    public void onButtonNewAction() {
-
-    }
-
-    private void initializeNodes() {
-        departmentIdTableColumn.setCellValueFactory(new PropertyValueFactory<>("Id"));
-        departmentNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
-
-        Stage stage = (Stage) Main.getMainScene().getWindow();
-        departmentTableView.prefHeightProperty().bind(stage.heightProperty());
+    public void onButtonNewAction(ActionEvent actionEvent) {
+        Stage parentStage = Utils.currentStage(actionEvent);
+        createDialogForm("/gui/DepartmentForm.fxml", parentStage);
     }
 
     public void updateTableView() {
@@ -58,6 +60,31 @@ public class DepartmentListController implements Initializable {
         List<Department> list = departmentService.findAll();
         observableList = FXCollections.observableArrayList(list);
         departmentTableView.setItems(observableList);
+    }
+
+    private void createDialogForm(String path, Stage parentStage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(path));
+            Pane pane = fxmlLoader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter department data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view.", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private void initializeNodes() {
+        departmentIdTableColumn.setCellValueFactory(new PropertyValueFactory<>("Id"));
+        departmentNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
+
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        departmentTableView.prefHeightProperty().bind(stage.heightProperty());
     }
 
     @Override
